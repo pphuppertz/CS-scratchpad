b244fef1461fa34fd5ded265007c88c971ff97a9
Peter Huppertz <peter.huppertz@yuki.nl>
2021-10-22 09:33:07 +0200

    Initial version

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS_scratchpad
+{
+	 class Program
+	 {
+		  static void Main(string[] args)
+		  {
+				Console.WriteLine("This is a scratchpad C# .NET 5 app, mainly to play around with GitKraken.");
+		  }
+	 }
+}
